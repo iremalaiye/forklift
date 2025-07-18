ee15c2e911582e47ed8d3c6b357fee9dc28a71b3
İrem Alaiye <iremalaiye@posta.mu.edu.tr>
2025-07-18 17:31:51 +0300

    logo added

--- resources/views/frontend/inc/header.blade.php
+++ resources/views/frontend/inc/header.blade.php
@@ -4,9 +4,8 @@
             <div class="row align-items-center">
 
                 <div class="col-6 col-md-4 order-2 order-md-1 site-search-icon text-left">
-
+                        <img src="{{ asset('images/icon.ico') }}" alt="Logo" style="height: 40px;">
                 </div>
-
                 <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
                     <div class="site-logo">
                         <a href="{{route('anasayfa')}}" class="js-logo-clone">{{config('app.name')}}</a>
