8377fdb5a54b27050cf56e727fdeef1beda64cb3
İrem Alaiye <iremalaiye@posta.mu.edu.tr>
2025-07-24 13:16:34 +0300

    slider name added

--- resources/views/backend/inc/sidebar.blade.php
+++ resources/views/backend/inc/sidebar.blade.php
@@ -10,12 +10,12 @@
         <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
                 <i class="icon-layout menu-icon"></i>
-                <span class="menu-title">Anasayfa</span>
+                <span class="menu-title">Slayt</span>
                 <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="{{route('panel.slider.index')}}">Anasayfa İçeriği</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{route('panel.slider.index')}}">Slayt İçeriği</a></li>
 
 
                 </ul>
